c7cfade307601c49fd06d77ea484930479ec79db
Rahul <rahulkrishlalwani@gmail.com>
2025-07-12 20:29:15 +0530

    making navbar center

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { NavBar } from "./components/Navbar";
 export default function Home() {
   
   return (
-    <div className="min-h-screen transition-all duration-100 dark:bg-background-black bg-white">
-      <NavBar height="h-24" />
+    <div className="flex justify-center min-h-screen transition-all duration-100 dark:bg-background-black bg-white">
+      <div className="w-3xl"><NavBar height="h-24"/></div>
     </div>
   );
 }
