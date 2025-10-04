cc48a2158d3ac151487422b89d3c6364d67a1138
Rahul <rahulkrishlalwani@gmail.com>
2025-10-04 21:24:56 +0530

    Added the special hover effect on links in project Card

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -15,7 +15,7 @@ export interface cardProps{
     priority?: boolean
 }
 export function ProjectCard({name, description, image, tags , links, priority = false}: cardProps){
-    return <div className="max-w-[360px] w-full px-8 py-8 flex flex-col gap-2 border dark:border-hover-black border-light-border rounded-lg shadow-lg">
+    return <div className="group max-w-[360px] w-full px-8 py-8 flex flex-col gap-2 border dark:border-hover-black border-light-border rounded-lg shadow-lg">
         <div className='max-w-2xs h-42 flex overflow-hidden m-auto rounded-sm'>
             <Image 
             src={image} 
@@ -36,7 +36,14 @@ export function ProjectCard({name, description, image, tags , links, priority = 
             {tags.map((x, i) => <Tag key={i} name={x}/>)}
         </div>
         <div className='flex flex-wrap justify-start gap-2'>
-            {links.map((x, i)=> <CardLink key={i} name={x.name} icon={x.icon} link={x.href}/>)}
+            {links.map((x, i)=> (
+                <div 
+                    key={i} 
+                    className="relative overflow-hidden transition-all duration-300 ease-in-out group-hover:bg-gradient-to-br group-hover:from-blue-500/25 group-hover:via-blue-600/15 group-hover:to-cyan-500/10 group-hover:border-blue-400/80 group-hover:shadow-lg group-hover:shadow-blue-500/20 border border-transparent rounded-lg p-[2px] before:absolute before:inset-0 before:bg-gradient-to-r before:from-transparent before:via-white/20 before:to-transparent before:translate-x-[-100%] group-hover:before:translate-x-[100%] before:transition-transform before:duration-700 before:ease-out"
+                >
+                    <CardLink name={x.name} icon={x.icon} link={x.href}/>
+                </div>
+            ))}
         </div>
     </div>
 }
